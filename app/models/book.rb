f54c17ac484204794186bf2f82436330e3d39bbf
matt <mattevo10@gmail.com>
2021-04-09 18:17:58 +0100

    rich text working

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,8 @@ class Book < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
   has_rich_text :rich_summary
+  has_rich_text :rich_notes
   
-  validates :title, :author, :summary, :rich_summary, :notes, :rating, presence: true
+  validates :title, :author, :rich_summary, :rich_notes, :rating, presence: true
   validates :rating, inclusion: { in: 0..10 }
 end
